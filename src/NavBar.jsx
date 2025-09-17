a413ef3690bbfa1fe921f05d9fe640429dc0214e
ahmedshaban-blip <ahmedshabanhassanahmed1234@gmail.com>
2025-09-17 12:43:09 +0300

    Edit Paths for surprise me and Browse in navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -12,12 +12,12 @@ import {
 } from '@coreui/react';
 import { Link, NavLink } from 'react-router-dom';
 import './NavBar.css';
- import { setLang } from './redux/Reducers/i18nReducer';
+import { setLang } from './redux/Reducers/i18nReducer';
 
 export const NavBarComponent = () => {
   const favCount = useSelector((state) => state.fav.items.length);
-   const lang = useSelector((state) => state.i18n.lang);
-   const dispatch = useDispatch();
+  const lang = useSelector((state) => state.i18n.lang);
+  const dispatch = useDispatch();
   const [visible, setVisible] = useState(false);
 
 
@@ -36,33 +36,33 @@ export const NavBarComponent = () => {
 
 
 
-   const t = {
-     en: {
-       home: 'Home',
-       browse: 'Browse',
-       favorites: 'Favorites',
-       surprise: '🎬 Surprise Me',
-       login: 'Login',
-       signup: 'Sign Up',
-       tagline: 'Your nightly movie fix 🍿',
-       langBtn: 'AR',
-     },
-     ar: {
-       home: 'الرئيسية',
-       browse: 'تصفح',
-       favorites: 'المفضلة',
-       surprise: '🎬 فاجئني',
-       login: 'تسجيل الدخول',
-       signup: 'إنشاء حساب',
-       tagline: 'جرعتك الليلية من الأفلام 🍿',
-       langBtn: 'EN',
-     },
-   }[lang];
+  const t = {
+    en: {
+      home: 'Home',
+      browse: 'Browse',
+      favorites: 'Favorites',
+      surprise: '🎬 Surprise Me',
+      login: 'Login',
+      signup: 'Sign Up',
+      tagline: 'Your nightly movie fix 🍿',
+      langBtn: 'AR',
+    },
+    ar: {
+      home: 'الرئيسية',
+      browse: 'تصفح',
+      favorites: 'المفضلة',
+      surprise: '🎬 فاجئني',
+      login: 'تسجيل الدخول',
+      signup: 'إنشاء حساب',
+      tagline: 'جرعتك الليلية من الأفلام 🍿',
+      langBtn: 'EN',
+    },
+  }[lang];
 
-   const toggleLang = () => {
-     const next = lang === 'en' ? 'ar' : 'en';     dispatch(setLang(next));
-     try { localStorage.setItem('lang', next); } catch {}
-   };
+  const toggleLang = () => {
+    const next = lang === 'en' ? 'ar' : 'en'; dispatch(setLang(next));
+    try { localStorage.setItem('lang', next); } catch { }
+  };
 
 
 
@@ -83,35 +83,35 @@ export const NavBarComponent = () => {
         <CCollapse className="navbar-collapse" visible={visible}>
           {/* */}
           <CNavbarNav className="me-auto">
-          
-                <NavLink end to="/" className="nav-link nav-link-custom">{t.home}</NavLink>
-             <NavLink to="/movies" className="nav-link nav-link-custom">{t.browse}</NavLink>
-             <NavLink to="/favorites" className="nav-link nav-link-custom position-relative">
-               {t.favorites}
+
+            <NavLink end to="/" className="nav-link nav-link-custom">{t.home}</NavLink>
+            <NavLink to="/" className="nav-link nav-link-custom">{t.browse}</NavLink>
+            <NavLink to="/favorites" className="nav-link nav-link-custom position-relative">
+              {t.favorites}
               {favCount > 0 && <span className="fav-badge">{favCount}</span>}
             </NavLink>
 
             {/* */}
-            
-            <Link to="/random" className="btn btn-outline-warning ms-lg-2 my-2 my-lg-0">{t.surprise}</Link>
+
+            <Link to="/" className="btn btn-outline-warning ms-lg-2 my-2 my-lg-0">{t.surprise}</Link>
 
           </CNavbarNav>
 
           {/* */}
           <div className="d-flex align-items-center gap-2 ms-auto">
 
-             <span className="navbar-tagline d-none d-lg-inline">{t.tagline}</span>
-             <CButton color="secondary" variant="outline" onClick={toggleLang} title="Toggle language">
-               {t.langBtn}
-             </CButton>
+            <span className="navbar-tagline d-none d-lg-inline">{t.tagline}</span>
+            <CButton color="secondary" variant="outline" onClick={toggleLang} title="Toggle language">
+              {t.langBtn}
+            </CButton>
             <Link to="/login" className="text-decoration-none">
 
-                             <CButton color="secondary" variant="outline">{t.login}</CButton>
+              <CButton color="secondary" variant="outline">{t.login}</CButton>
 
             </Link>
 
             <Link to="/signup" className="text-decoration-none">
-               <CButton color="info">{t.signup}</CButton>
+              <CButton color="info">{t.signup}</CButton>
             </Link>
           </div>
         </CCollapse>
